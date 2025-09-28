124ab0464d9a353159cec00902a8a5e7ecd663a2
Adesh-nagare <anagare@algoanalytics.com>
2025-09-28 18:18:16 +0530

    Api issue resolved

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,14 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
-  withCredentials: true,
+});
+
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default instance;
